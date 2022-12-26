1e8c7b050fa5833278d6de3ca6473191d41c2854
kaschaldak <120067174+kaschaldak@users.noreply.github.com>
2022-12-26 13:43:18 +0500

    Palindrome

---
+++ a_task_1/Program.cs
@@ -0,0 +1,23 @@
+﻿// Задача 19/ Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+// 14212 -> нет
+//12821 -> да
+//23432 -> да
+Console.WriteLine("Enter SetNumber : ");
+int SetNumber = Convert.ToInt32(Console.ReadLine ());
+int  numPalindrome  =  SetNumber ( "Palindrome " );
+
+bool  isPalindrome  =  IsPalindromeInt( numPalindrome );
+
+// bool isPalindrome = IsPalindromeString(numPalindrome.ToString());
+
+string str  =  isPalindrome  ?  " является палиндромом "  :  " не является палиндромом " ;
+
+if (  isPalindrome )
+{
+    str  =  " является палиндромом " ;
+}
+else
+{
+    str  =  " не является палиндромом " ;
+}
+System.Console. WriteLine ( $" число { numPalindrome } { str } " );
