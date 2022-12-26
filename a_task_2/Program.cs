7720a8dbb6bf5a548fde40857ff9f0bf86ece3b9
kaschaldak <120067174+kaschaldak@users.noreply.github.com>
2022-12-26 14:57:03 +0500

    cube number

--- a_task_2/Program.cs
+++ a_task_2/Program.cs
@@ -1,21 +1,6 @@
 ﻿// Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
 //A (3,6,8); B (2,1,-7), -> 15.84
-// A (7,-5, 0); B (1,-1,9) -> 11.53
-Console.WriteLine("введите координаты точек");
-Console.WriteLine( " x1 : "  );
-int  x1= Convert.ToInt32 (Console.ReadLine());
-Console.WriteLine( " y1 : "  );
-int  y1= Convert.ToInt32 (Console.ReadLine());
-Console.WriteLine( " z1 : "  );
-int  z1= Convert.ToInt32 (Console.ReadLine());
-Console.WriteLine( " x2 : "  );
-int  x2= Convert.ToInt32 (Console.ReadLine());
-Console.WriteLine( " y2 : "  );
-int  y2= Convert.ToInt32 (Console.ReadLine());
-Console.WriteLine( " z2 : "  );
-int  z2= Convert.ToInt32 (Console.ReadLine());
-
-
+// A (7,-5, 0); B (1,-1,9) -> 11.5
 
 int  x1  =  SetNumber ( " x1 " );
 int  y1  =  SetNumber ( " y1 " );
@@ -24,7 +9,15 @@ int  x2  =  SetNumber ( " x2 " );
 int  y2  =  SetNumber ( " y2 " );
 int  z2  =  SetNumber ( " z2 " );
 
-double result   =  DistanceBetween3DPoints ( x1 , x2 , y1 , y2 , z1 , z2 );
+int  SetNumber ( string   numberName )
+{
+    Console.WriteLine ( $" Введите число  { numberName }: " );
+    int num   =  Convert . ToInt32 ( Console.ReadLine ( ) ) ;
+    return num;
+}
 
-Console. WriteLine ( $" result { результат } " );
-/* конец */
+double  DistanceBetween3DPoints ( int  x1 , int  x2 , int  y1 , int  y2 , int  z1 , int  z2 )
+{
+    return Math . Sqrt ( Math.Pow (( x2 - x1 ) , 2 ) + Math.Pow (( y2 - y1 ) , 2 ) + Math.Pow ( ( z2 - z1 ) , 2 ) ) ;        
+}
+Console. WriteLine ( $" Расстояние между точками={ distanceBetweenPoints : f2 } " );
